543737a93c868a9bdac35241c2b89fa5b0d25bc4
trainlock <trainlock@gmail.com>
2020-09-12 19:10:42 +0200

    Updates

---
+++ precache-manifest.77c60e49173677767a066a713a14a51a.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1931239befca4e14b6c7e0896dd36c4c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f2287e6879857c4f0d81",
+    "url": "/portfolio/static/css/2.c0a403c7.chunk.css"
+  },
+  {
+    "revision": "c859c6ca20d9346d3746",
+    "url": "/portfolio/static/css/main.0d72b2d6.chunk.css"
+  },
+  {
+    "revision": "f2287e6879857c4f0d81",
+    "url": "/portfolio/static/js/2.4bfab55b.chunk.js"
+  },
+  {
+    "revision": "cf7302ab57415842ba2bf20f719dee93",
+    "url": "/portfolio/static/js/2.4bfab55b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c859c6ca20d9346d3746",
+    "url": "/portfolio/static/js/main.cf4ade00.chunk.js"
+  },
+  {
+    "revision": "309a9e55c61ba5bff665",
+    "url": "/portfolio/static/js/runtime-main.b3df5f95.js"
+  },
+  {
+    "revision": "f2a36e9e4d65f80d0cd4b74a32db8a5e",
+    "url": "/portfolio/static/media/LookForMyCompany.f2a36e9e.png"
+  },
+  {
+    "revision": "0d52623af709de10c1a98200e68cd475",
+    "url": "/portfolio/static/media/LookForMyHomeHover.0d52623a.png"
+  },
+  {
+    "revision": "93d5f6bbb3c2dc8696d72a1d099e8044",
+    "url": "/portfolio/static/media/bary-triangle.93d5f6bb.png"
+  },
+  {
+    "revision": "6f1d5bcf512466faf8951dbde5925008",
+    "url": "/portfolio/static/media/cow.6f1d5bcf.png"
+  },
+  {
+    "revision": "c3ce60b8242b6b2faf366e2f5586b91e",
+    "url": "/portfolio/static/media/disp-map.c3ce60b8.png"
+  },
+  {
+    "revision": "63612ffe668bd90f87bf823f97457630",
+    "url": "/portfolio/static/media/face-half.63612ffe.png"
+  },
+  {
+    "revision": "fb0b9500ae6b2aa48013fa6c14d817dc",
+    "url": "/portfolio/static/media/face-neutral-wire.fb0b9500.png"
+  },
+  {
+    "revision": "138cf263dcbc6a48e3a57bf54f1d891c",
+    "url": "/portfolio/static/media/face-tension.138cf263.png"
+  },
+  {
+    "revision": "619118cb8af3824a4870e73df3965629",
+    "url": "/portfolio/static/media/flubber.619118cb.png"
+  },
+  {
+    "revision": "f7ade84db39435a717b82d3a15e8cd4c",
+    "url": "/portfolio/static/media/forest-lake-vivid.f7ade84d.jpg"
+  },
+  {
+    "revision": "2579bb49f797ed7a3b969192e27c937d",
+    "url": "/portfolio/static/media/haystack-1.2579bb49.png"
+  },
+  {
+    "revision": "6cb98d1b5a5abc6d14eee02c935249de",
+    "url": "/portfolio/static/media/haystack-2.6cb98d1b.png"
+  },
+  {
+    "revision": "9d69057455238eb2999e621970dbbc1f",
+    "url": "/portfolio/static/media/hibyebook-web.9d690574.png"
+  },
+  {
+    "revision": "4451fcb0414b430a27e483eea5a3cc46",
+    "url": "/portfolio/static/media/hibyebook.4451fcb0.png"
+  },
+  {
+    "revision": "88210261a0c5c4d06d609f7e50af12ce",
+    "url": "/portfolio/static/media/lamp.88210261.png"
+  },
+  {
+    "revision": "8102c2e32cc8b0f4ab78ffdc1f854abf",
+    "url": "/portfolio/static/media/legionenTheGame.8102c2e3.png"
+  },
+  {
+    "revision": "eb2d46f65e73e3cbd643d5ede99d5cd9",
+    "url": "/portfolio/static/media/legionenTheGamePlaying.eb2d46f6.png"
+  },
+  {
+    "revision": "ef289c1954c787434d17edf5920611dc",
+    "url": "/portfolio/static/media/librarian.ef289c19.png"
+  },
+  {
+    "revision": "91d0699c262b190e4996d1e2848bc066",
+    "url": "/portfolio/static/media/linnea-grayscale.91d0699c.jpg"
+  },
+  {
+    "revision": "ab1c3a84c69493fc6efde2107cbfdbf7",
+    "url": "/portfolio/static/media/linnea-sun.ab1c3a84.jpeg"
+  },
+  {
+    "revision": "0036521cc85c5bb304921836bdebdbfe",
+    "url": "/portfolio/static/media/lookForMy.0036521c.jpg"
+  },
+  {
+    "revision": "f034efc1791e91fb2dd8dd42e9e2ffe7",
+    "url": "/portfolio/static/media/masterThesis.f034efc1.png"
+  },
+  {
+    "revision": "bd99a41363e9cfd3294700b77eccba1c",
+    "url": "/portfolio/static/media/matlab.bd99a413.png"
+  },
+  {
+    "revision": "1f8051ea1af0354199276ffbb3fa1c38",
+    "url": "/portfolio/static/media/mill.1f8051ea.png"
+  },
+  {
+    "revision": "b6e203235878afa896d44df58aecdbb7",
+    "url": "/portfolio/static/media/omr.b6e20323.png"
+  },
+  {
+    "revision": "f034efc1791e91fb2dd8dd42e9e2ffe7",
+    "url": "/portfolio/static/media/render-dynamic-1121.f034efc1.png"
+  },
+  {
+    "revision": "7175ecc07e312a58e7d93d8ba8d7fb15",
+    "url": "/portfolio/static/media/render-full-face.7175ecc0.png"
+  },
+  {
+    "revision": "e9af38b3cfb5191e03928bbe46779990",
+    "url": "/portfolio/static/media/render-gif-2211.e9af38b3.gif"
+  },
+  {
+    "revision": "79724f04765100988ae69984dbe0c2d5",
+    "url": "/portfolio/static/media/siege.79724f04.png"
+  },
+  {
+    "revision": "37d55930e2786816878ed68cebe7857a",
+    "url": "/portfolio/static/media/springs.37d55930.jpg"
+  },
+  {
+    "revision": "d5d26cfedbb90bc957e7d597267a1333",
+    "url": "/portfolio/static/media/stair.d5d26cfe.png"
+  },
+  {
+    "revision": "9c17c366421efae99e0f837a761ba9c0",
+    "url": "/portfolio/static/media/survey.9c17c366.png"
+  },
+  {
+    "revision": "c25530c8ac0ace8ba7260fe41516f47b",
+    "url": "/portfolio/static/media/test.c25530c8.jpg"
+  },
+  {
+    "revision": "41c5fffd654c3b86f42f8dc3b0511300",
+    "url": "/portfolio/static/media/tncg13-cube.41c5fffd.png"
+  },
+  {
+    "revision": "17370baab7b3e7164bd1c4a00cff05b1",
+    "url": "/portfolio/static/media/tncg13-flubber.17370baa.png"
+  },
+  {
+    "revision": "d44b3cd79745c6025a82d4a084d22aa7",
+    "url": "/portfolio/static/media/tncg13-pos.d44b3cd7.png"
+  },
+  {
+    "revision": "e59a8ab9c027259594b1ecbb1eb1a5ed",
+    "url": "/portfolio/static/media/tncg13-teapot.e59a8ab9.png"
+  },
+  {
+    "revision": "71cea3c474ea6d8dd3bbd53a65715d69",
+    "url": "/portfolio/static/media/tnm069-logo.71cea3c4.png"
+  },
+  {
+    "revision": "111d5f3aa76bf1b1b27be0f571210464",
+    "url": "/portfolio/static/media/tnm069-style.111d5f3a.png"
+  },
+  {
+    "revision": "f3d476d6b909f8d1f65b69dc11806efb",
+    "url": "/portfolio/static/media/tnm069-web.f3d476d6.png"
+  },
+  {
+    "revision": "dfea165e2e27517c67193185459b0180",
+    "url": "/portfolio/static/media/tnm079-lab1.dfea165e.png"
+  },
+  {
+    "revision": "c4981e2c4fa97905a6cb9fd2aa9bf8cf",
+    "url": "/portfolio/static/media/tnm079-lab2-cow.c4981e2c.png"
+  },
+  {
+    "revision": "1c7963e9a5e0f7356f5fef7dced59c93",
+    "url": "/portfolio/static/media/tnm079-lab4-1.1c7963e9.png"
+  },
+  {
+    "revision": "19573105389be38176614f8a79f9046e",
+    "url": "/portfolio/static/media/tnm079-lab4-2.19573105.png"
+  },
+  {
+    "revision": "c91795c33b34dbf997f2a8bfa1e1c4b7",
+    "url": "/portfolio/static/media/tnm079-lab4-3.c91795c3.png"
+  },
+  {
+    "revision": "81a19104f206a24087690f2041ebfcbf",
+    "url": "/portfolio/static/media/tnm079-lab4-4.81a19104.png"
+  },
+  {
+    "revision": "2c9b077e20a3b8dc5eccabe815353c17",
+    "url": "/portfolio/static/media/tnm079-lab5-level.2c9b077e.png"
+  },
+  {
+    "revision": "6a43ba705f07ed4feaf5a344ff73f9fd",
+    "url": "/portfolio/static/media/tnm079-lab5-teapot.6a43ba70.png"
+  },
+  {
+    "revision": "bf1b3b747d07aac00f9f5633d6af5300",
+    "url": "/portfolio/static/media/tnm079-lab6.bf1b3b74.png"
+  },
+  {
+    "revision": "355956e81eefa5fd6dfb12a2e281a318",
+    "url": "/portfolio/static/media/tnm094-card.355956e8.png"
+  },
+  {
+    "revision": "aac3e9cd2894e977fa434a7d1b461b16",
+    "url": "/portfolio/static/media/tnm094-game.aac3e9cd.png"
+  },
+  {
+    "revision": "24f04a1f4945c43af8a5f50b3b54806c",
+    "url": "/portfolio/static/media/tnm094-menuStartQuit.24f04a1f.png"
+  },
+  {
+    "revision": "c1a625999c18f05e6817d6a33af44d21",
+    "url": "/portfolio/static/media/tnm094-models.c1a62599.jpg"
+  },
+  {
+    "revision": "e14948fa20f4189bac6c87beea54ef4e",
+    "url": "/portfolio/static/media/tnm094-siegeCard.e14948fa.png"
+  },
+  {
+    "revision": "76de484a880aa229605fa15834defb28",
+    "url": "/portfolio/static/media/tsbk35-astar.76de484a.png"
+  },
+  {
+    "revision": "7a37bcab30f914b60a425b8f4b03cd13",
+    "url": "/portfolio/static/media/tsbk35-game.7a37bcab.png"
+  },
+  {
+    "revision": "d3f30965011af0d233acc1606c00dcbe",
+    "url": "/portfolio/static/media/tsbk35-path.d3f30965.png"
+  },
+  {
+    "revision": "c8c616bc2520ff2d9280b5ae1807d51b",
+    "url": "/portfolio/static/media/tygladig.c8c616bc.png"
+  },
+  {
+    "revision": "b7e42cf57bb57ed0a83aa4833fa523dc",
+    "url": "/portfolio/static/media/windmill.b7e42cf5.png"
+  }
+]);
